07000a3e1a673d5f86b2137459b0a1d7f5c8fec6
Alexey <nerjs.stap@gmail.com>
2022-09-13 20:15:28 +0200

    remove all

--- src/Message.ts
+++
@@ -1,5 +0,0 @@
-import type { Format } from './Format'
-
-export class Message {
-  constructor(private readonly format: Format, private readonly data: any[]) {}
-}
--- src/utils/object.ts
+++ src/utils/object.ts
@@ -11,20 +11,3 @@ export const iterateKeys = (keyPath: string | string[], obj: object, fn: (key: s
   if (typeof obj[first] !== 'object') return
   return iterateKeys(keys, obj[first], fn)
 }
-
-export const renameField = (keyPath: string | string[], obj: object, fn: (key: string, val: any) => string | null | undefined) => {
-  iterateKeys(keyPath, obj, (key, nobj) => {
-    const nkey = fn(key, nobj[key])
-    if (!nkey) return
-    nobj[nkey] = nobj[key]
-    delete nobj[key]
-  })
-}
-
-export const revalueField = (keyPath: string | string[], obj: object, fn: (key: string, val: any) => any | undefined) => {
-  iterateKeys(keyPath, obj, (key, nobj) => {
-    const nval = fn(key, nobj[key])
-    if (nval === undefined) return
-    nobj[key] = nval 
-  })
-}
